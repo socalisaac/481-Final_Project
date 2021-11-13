9e2c7cbf4f1ac67e0b65c5bbdd0e8ff1f6600603
socalisaac <socalisaac@yahoo.com>
2021-11-13 12:09:16 -0800

    create other file components including games.py, variables.py, and genetic.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CPSC_481-Final_Project
+
+Project idea: Mastermind
+
+Team members:
+
+Shervin Afrasiabi, Isaac Estrada, Kevin Dillon
--- mastermind.py
+++ mastermind.py
@@ -2,7 +2,11 @@ import os
 import random
 import sys
 
-class Mastermind():
+from game import *
+from genetic import *
+from variables import *
+
+class Mastermind(game):
     def __init__():
         print("create mastermind game")
 
